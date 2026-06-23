e4e217e385c061b13652fa33383b19b9092a2fe4
capote86-ui <capote86@gmail.com>
2026-06-23 15:28:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,8 @@ from collections import defaultdict, deque, Counter
 from openai import OpenAI
 import os
 
-TOKEN = os.getenv("TOKEN")
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+TOKEN = "8996485412:AAF_SJkLwA3-3xtMUGj59TUJNGcF17J9LV0"
+print("API:", os.getenv("OPENAI_API_KEY"))
 print("API:", os.getenv("OPENAI_API_KEY"))
 respuestas = {
 
